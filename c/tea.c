2fd7dfa2fe783fd4177d8ff6fc9f813302df3868
kapo <newsletter_pvt@ldlabs.org>
2013-12-08 00:17:48 +0100

    added new c function for xtea, xxtea, rc4

--- c/tea.c
+++ c/tea.c
@@ -10,6 +10,45 @@ unsigned int    key[KEY] = {
     0x0, 0x0
 };
 
+#define DELTA 0x9e3779b9
+#define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
+
+// BTEA or XXTEA http://en.wikipedia.org/wiki/XXTEA 
+  void btea(uint32_t *v, int n, uint32_t const key[4]) {
+    uint32_t y, z, sum;
+    unsigned p, rounds, e;
+    if (n > 1) {          /* Coding Part */
+      rounds = 6 + 52/n;
+      sum = 0;
+      z = v[n-1];
+      do {
+        sum += DELTA;
+        e = (sum >> 2) & 3;
+        for (p=0; p<n-1; p++) {
+          y = v[p+1]; 
+          z = v[p] += MX;
+        }
+        y = v[0];
+        z = v[n-1] += MX;
+      } while (--rounds);
+    } else if (n < -1) {  /* Decoding Part */
+      n = -n;
+      rounds = 6 + 52/n;
+      sum = rounds*DELTA;
+      y = v[0];
+      do {
+        e = (sum >> 2) & 3;
+        for (p=n-1; p>0; p--) {
+          z = v[p-1];
+          y = v[p] -= MX;
+        }
+        z = v[n-1];
+        y = v[0] -= MX;
+      } while ((sum -= DELTA) != 0);
+    }
+  }
+
+
 void test()
 {
     uint32_t sum = 0;
@@ -45,7 +84,8 @@ void test()
 
 }
 
-void encrypt (uint32_t* v, uint32_t* k) {
+// http://en.wikipedia.org/wiki/Tiny_Encryption_Algorithm
+void tea_encrypt (uint32_t* v, uint32_t* k) {
     uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
     uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
     uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
@@ -58,7 +98,7 @@ void encrypt (uint32_t* v, uint32_t* k) {
     //printf("%02i %08lX %08lX\n", v0, v1);
 }
  
-void decrypt (uint32_t* v, uint32_t* k) {
+void tea_decrypt (uint32_t* v, uint32_t* k) {
     uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;  /* set up */
     uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
     uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
@@ -69,6 +109,29 @@ void decrypt (uint32_t* v, uint32_t* k) {
     }                                              /* end cycle */
     v[0]=v0; v[1]=v1;
     //printf("%02i %08lX %08lX\n", v0, v1);
+}
+
+// XTEA http://en.wikipedia.org/wiki/XTEA
+void xtea_encrypt(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
+    unsigned int i;
+    uint32_t v0=v[0], v1=v[1], sum=0, delta=0x9E3779B9;
+    for (i=0; i < num_rounds; i++) {
+        v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+        sum += delta;
+        v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+    }
+    v[0]=v0; v[1]=v1;
+}
+
+void xtea_decrypt(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
+    unsigned int i;
+    uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*num_rounds;
+    for (i=0; i < num_rounds; i++) {
+        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+        sum -= delta;
+        v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+    }
+    v[0]=v0; v[1]=v1;
 }
 
 int main() {
