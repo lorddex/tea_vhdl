1ec43b658e7f8a9cfba40865ad5e0f49eaa82207
kapo <newsletter_pvt@ldlabs.org>
2013-12-21 16:17:15 +0100

    tea.c final release

--- c/tea.c
+++ c/tea.c
@@ -91,44 +91,6 @@ void arc4_encrypt(uint32_t *v, uint32_t *key, int len)
     arc4_prga(state, (uint8_t*)v, len*4);
 }
 
-// BTEA or XXTEA http://en.wikipedia.org/wiki/XXTEA 
-void xxtea_encode(uint32_t *v, uint8_t *key) {
-    int n = 2; // NUM WORDS TO ENCODE
-    uint32_t y, z, sum;
-    unsigned p, rounds, e;
-    rounds = 6 + 52/n;
-    sum = 0;
-    z = v[n-1];
-    do {
-        sum += DELTA;
-        e = (sum >> 2) & 3;
-        for (p=0; p<n-1; p++) {
-          y = v[p+1]; 
-          z = v[p] += (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)));
-        }
-        y = v[0];
-        z = v[n-1] += (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)));
-    } while (--rounds);
-  }
-
-void xxtea_decode(uint32_t *v, uint8_t *key) {
-    int n = 2; // NUM WORDS TO DECODE
-    uint32_t y, z, sum;
-    unsigned p, rounds, e;
-    rounds = 6 + 52/n;
-    sum = rounds*DELTA;
-    y = v[0];
-    do {
-        e = (sum >> 2) & 3;
-        for (p=n-1; p>0; p--) {
-          z = v[p-1];
-          y = v[p] -= (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)));
-        }
-        z = v[n-1];
-        y = v[0] -= (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)));
-    } while ((sum -= DELTA) != 0);
-}
-
 // http://en.wikipedia.org/wiki/Tiny_Encryption_Algorithm
 void tea_encrypt (uint32_t* v, uint8_t* k) {
     uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
@@ -138,7 +100,7 @@ void tea_encrypt (uint32_t* v, uint8_t* k) {
         sum += delta;
         v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
         v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);  
-	printf("Tea encoded values: v0=%08"PRIX8" v1=%08"PRIX8"\n", v0, v1);
+//	printf("Tea encoded values: v0=%08"PRIX8" v1=%08"PRIX8"\n", v0, v1);
     }                                              /* end cycle */
     v[0]=v0; v[1]=v1;
 }
@@ -152,29 +114,6 @@ void tea_decrypt (uint32_t* v, uint8_t* k) {
         v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
         sum -= delta;                                   
     }                                              /* end cycle */
-    v[0]=v0; v[1]=v1;
-}
-
-// XTEA http://en.wikipedia.org/wiki/XTEA
-void xtea_encrypt(uint32_t *v, uint8_t *key) {
-    unsigned int i;
-    uint32_t v0=v[0], v1=v[1], sum=0, delta=DELTA;
-    for (i=0; i < NUM_ROUNDS; i++) {
-        v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-        sum += delta;
-        v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
-    }
-    v[0]=v0; v[1]=v1;
-}
-
-void xtea_decrypt(uint32_t *v, uint8_t *key) {
-    unsigned int i;
-    uint32_t v0=v[0], v1=v[1], delta=DELTA, sum=delta*NUM_ROUNDS;
-    for (i=0; i < NUM_ROUNDS; i++) {
-        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
-        sum -= delta;
-        v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-    }
     v[0]=v0; v[1]=v1;
 }
 
@@ -196,20 +135,6 @@ int main() {
 
     tea_decrypt(v, key);
     printf("Tea decoded values: v0=%08"PRIX8" %08"PRIX8"\n", v[0], v[1]);
-#elif TEA == 2
-    /* XTEA */
-    xtea_encrypt(v, key);
-    printf("XTea encoded values: v0=%08"PRIX8" v1=%08"PRIX8"\n", v[0], v[1]);
-
-    xtea_decrypt(v, key);
-    printf("XTea decoded values: v0=%08"PRIX8" %08"PRIX8"\n", v[0], v[1]);
-#elif TEA == 3
-    /* XXTEA */
-    xtea_encrypt(v, key);
-    printf("XXTea encoded values: v0=%08"PRIX8" v1=%08"PRIX8"\n", v[0], v[1]);
-
-    xtea_decrypt(v, key);
-    printf("XXTea decoded values: v0=%08"PRIX8" %08"PRIX8"\n", v[0], v[1]);
 #endif
 
 #if STREAM == 1
